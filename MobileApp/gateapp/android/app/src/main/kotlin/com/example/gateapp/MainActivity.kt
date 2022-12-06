d19617fd2e23fc50b377792ca199a61f24799ade
Ygnas <ygnas@outlook.com>
2022-12-06 00:26:33 +0000

    flutter android application

---
+++ MobileApp/gateapp/android/app/src/main/kotlin/com/example/gateapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gateapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ MobileApp/gateapp/lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+
+void main() async {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  final channel = WebSocketChannel.connect(
+    Uri.parse('ws://192.168.0.138:8000'),
+  );
+
+  final textController = TextEditingController();
+
+  void dispose() {
+    channel.sink.close();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.deepPurple[300],
+        body: StreamBuilder(
+          stream: channel.stream,
+          builder: (context, snapshot) {
+            textController.text = snapshot.hasData ? '${snapshot.data}' : '';
+            return Column(children: [
+              const SizedBox(
+                height: 350,
+              ),
+              Center(
+                child: Text(
+                  textController.text,
+                  style: const TextStyle(fontSize: 25),
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Container(
+                child: textController.text == "Can Be Opened"
+                    ? FloatingActionButton.extended(
+                        onPressed: () {
+                          if (textController.text == "Can Be Opened") {
+                            channel.sink.add('open');
+                          }
+                        },
+                        label: const Text("Open Gate"),
+                        icon: const Icon(Icons.lock_open_rounded),
+                      )
+                    : FloatingActionButton.extended(
+                        onPressed: () {
+                          if (textController.text == "Can Be Opened") {
+                            channel.sink.add('open');
+                          }
+                        },
+                        label: const Text("🤯"),
+                        icon: const Icon(Icons.lock_rounded),
+                        backgroundColor: Colors.red,
+                      ),
+              ),
+            ]);
+          },
+        ),
+      ),
+    );
+  }
+}
