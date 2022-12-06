bb638e1227fed9faf72fc4a29519b85e4e3f40fc
Ygnas <ygnas@outlook.com>
2022-12-06 12:52:18 +0000

    extra comments

--- RaspberryPi/server.py
+++ RaspberryPi/server.py
@@ -12,6 +12,7 @@ from datetime import datetime
 connected = set()
 reply = "Can Be Opened"
 
+# serviceAccountKey.json is needed to be able to connect to firebase
 cred=credentials.Certificate('./serviceAccountKey.json')
 firebase_admin.initialize_app(cred, {
     'storageBucket': 'pipi-6383e.appspot.com',
@@ -41,13 +42,17 @@ mqttc.connect("192.168.0.138", 1883)
 mqttc.loop_start()
 
 async def echo(websocket, path):
+    # So it would use global reply variable and not make one local to the function
     global reply
+    # Adds clients to connected clients
     connected.add(websocket)
     try:
         await broadcast(0)
         # Broadcast a message to all connected clients.
         async for message in websocket:
+            # Published message to MQTT broker
             mqttc.publish("gates", "off")
+            # Sends data to firebase with message
             push_db("Gate Open")
             reply = "Openning"
             print("Received: " + message)
@@ -61,6 +66,7 @@ async def echo(websocket, path):
         # Unregister.
         connected.remove(websocket)
 
+# Broadcasts message to all clients connected
 async def broadcast(delay):
     websockets.broadcast(connected, reply)
     await asyncio.sleep(delay)
@@ -70,7 +76,7 @@ def push_db(message):
     now = datetime.now()
     daytime = now.strftime("%d/%m/%Y %H:%M:%S")
 
-    # Push file reference to image in Realtime DB
+    # Push file reference to gate_log in Realtime DB
     home_ref.push({
         'message': message,
         'timestamp': daytime}
