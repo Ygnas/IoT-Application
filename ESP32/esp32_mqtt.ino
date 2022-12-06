0516de5f2ed8ea5c13b9df0b61985206bacc162e
Ygnas <ygnas@outlook.com>
2022-12-06 00:20:58 +0000

    esp32 mqtt listener

---
+++ ESP32/esp32_mqtt.ino
@@ -0,0 +1,86 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+const char* ssid     = "";
+const char* password = "";
+const char* mqtt_server = "";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+char msg[50];
+int value = 0;
+#define LED 2
+
+void setup() {
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, HIGH);
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+}
+
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* message, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println();
+  if (String(topic) == "gates") {
+    if(messageTemp == "off"){
+      digitalWrite(LED, LOW);
+      delay(1000);
+      digitalWrite(LED, HIGH);
+    }
+  }
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP32Client")) {
+      Serial.println("connected");
+      // Subscribe
+      client.subscribe("gates");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
